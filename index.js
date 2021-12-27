bf5a2de39509c40d8f5f65c683cf20fb1ca75fcf
Shin Yi <contact.shinyi@gmail.com>
2021-12-28 00:31:18 +0800

    added mongoose connection

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 const mongoose = require("mongoose");
-const apps = require("./app");
+const app = require("./app");
+const PORT = process.env.PORT || 8000;
 require("dotenv").config();
 
-const PORT = process.env.PORT || 8000;
+const MONGO_PWD = process.env.MONGO_PWD;
+const MONGO_DATABASE = process.env.MONGO_DATABASE;
+const MONGO_USER = process.env.MONGO_USER;
+const MONGO_BASE_URL = process.env.MONGO_BASE_URL;
+const MONGO_URL = `mongodb+srv://${MONGO_USER}:${MONGO_PWD}@${MONGO_BASE_URL}/${MONGO_DATABASE}?retryWrites=true&w=majority`;
 
-app.listen(PORT, () => {
-  console.log("listening on ", PORT);
+mongoose.connect(MONGO_URL).then(() => {
+  console.log("database connected");
+  app.listen(PORT, () => {
+    console.log("listening on", PORT);
+  });
 });
